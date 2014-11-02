1559aa414244abd3d32f6cc97971ad5a43653a47
Guido <guidopercu@gmail.com>
2014-11-02 17:36:23 -0200

    Reorganizando algoritmos

--- Importantes/String/hamming_distance.py
+++ Importantes/String/hamming_distance.py
@@ -31,4 +31,6 @@ def hamming_distance(s1, s2):
     return sum(ch1 != ch2 for ch1, ch2 in zip(s1, s2))
 
 
-print(hamming_distance('ACGT', 'AGCT'))
+string1 = input()
+string2 = input()
+print(hamming_distance(string1, string2))
